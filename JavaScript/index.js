c6896752a5ad9121c58686b8fde3f354c06adc4f
Nushrath Nazeer <nushrathnazeer2016@gmail.com>
2023-11-08 21:43:22 +0530

    add new file

---
+++ JavaScript/index.js
@@ -0,0 +1,29 @@
+//inserting something to the console
+console.log("Web Programming");
+
+
+
+//to print on an alert
+//alert('Nushrath'); 
+
+
+
+//to write in the doc
+document.writeln('Nushrath 1');
+document.write('<br>');//adding new line 
+document.write('Kandy');
+document.write('<h1>Hi peeps!</h1>');//adding heading style
+document.write('<button>Click me</button>');//adding button
+
+
+
+//global variable
+var name = 'Nush';
+
+//local variable
+{
+    var town="Kandy";
+}
+
+
+
